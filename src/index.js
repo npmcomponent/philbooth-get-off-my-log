1406da8f59dbb38096edc16b1bb4bc03f7f97a24
Phil Booth <pmbooth@gmail.com>
2013-12-08 14:27:52 +0000

    Update to work in browser (ES5 only).

--- src/index.js
+++ src/index.js
@@ -1,72 +1,88 @@
-'use strict'
+(function (globals) {
+    'use strict'
 
-var methods;
+    var functions, methods;
+    
+    functions = {
+        initialise: initialise
+    };
 
-module.exports = {
-    initialise: initialise
-};
+    exportFunctions();
+    
+    function initialise (origin, logger) {
+        var methodName, result = {};
+    
+        for (methodName in methods) {
+            if (methods.hasOwnProperty(methodName)) {
+                result[methodName] = methods[methodName].bind(null, logger || console.log.bind(console), origin);
+            }
+        }
+    
+        return result;
+    }
+    
+    methods = {
+        info: createLogMethod('INFO'),
+        warn: createLogMethod('WARN'),
+        error: createLogMethod('ERROR')
+    };
+    
+    function createLogMethod (level) {
+        return write.bind(null, level);
+    }
+    
+    function write (level, logger, origin, message) {
+        logger([
+            getTimestamp(),
+            level,
+            origin + ':',
+            message
+        ].join(' '));
+    }
+    
+    function getTimestamp () {
+        var time = new Date();
+    
+        return [
+            formatDate(time),
+            formatTime(time)
+        ].join(' ');
+    }
+    
+    function formatDate (time) {
+        return [
+            1900 + time.getYear(),
+            padNumber(time.getMonth() + 1),
+            padNumber(time.getDate())
+        ].join('-');
+    }
+    
+    function formatTime (time) {
+        return [
+            padNumber(time.getHours()),
+            padNumber(time.getMinutes()),
+            padNumber(time.getSeconds())
+        ].join(':');
+    }
+    
+    function padNumber (number) {
+        if (number < 10) {
+            return '0' + number;
+        }
+    
+        return number;
+    }
 
-function initialise (origin, logger) {
-    var methodName, result = {};
-
-    for (methodName in methods) {
-        if (methods.hasOwnProperty(methodName)) {
-            result[methodName] = methods[methodName].bind(null, logger || console.log.bind(console), origin);
+    function exportFunctions () {
+        if (typeof define === 'function' && define.amd) {
+            define(function () {
+                return functions;
+            });
+        } else if (typeof module !== 'undefined' && module !== null) {
+            module.exports = functions;
+        } else {
+            globals.log = functions;
         }
     }
-
-    return result;
-}
-
-methods = {
-    info: createLogMethod('INFO'),
-    warn: createLogMethod('WARN'),
-    error: createLogMethod('ERROR')
-};
-
-function createLogMethod (level) {
-    return write.bind(null, level);
-}
-
-function write (level, logger, origin, message) {
-    logger([
-        getTimestamp(),
-        level,
-        origin + ':',
-        message
-    ].join(' '));
-}
-
-function getTimestamp () {
-    var time = new Date();
-
-    return [
-        formatDate(time),
-        formatTime(time)
-    ].join(' ');
-}
-
-function formatDate (time) {
-    return [
-        1900 + time.getYear(),
-        padNumber(time.getMonth() + 1),
-        padNumber(time.getDate())
-    ].join('-');
-}
-
-function formatTime (time) {
-    return [
-        padNumber(time.getHours()),
-        padNumber(time.getMinutes()),
-        padNumber(time.getSeconds())
-    ].join(':');
-}
-
-function padNumber (number) {
-    if (number < 10) {
-        return '0' + number;
-    }
-
-    return number;
-}
+}(this));
 
